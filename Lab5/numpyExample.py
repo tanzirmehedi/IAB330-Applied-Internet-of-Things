c267ed14ddf752d3e1ba204dddf5133759298431
Sk. Tanzir Mehedi <23363707+tanzirmehedi@users.noreply.github.com>
2024-08-20 21:26:51 +1000

    Update

--- Lab5/numpyExample.py
+++ Lab5/numpyExample.py
@@ -6,4 +6,4 @@ data = [3, 7, 2, 9, 4]
 # Calculate the range as the difference between max and min
 range_value = np.max(data) - np.min(data)
 
-print("Range:", range_value) # Range: 6
+print("Range:", range_value) # Range: 7
